ffa061bf6be4e948623f40279deb162fd157d7bc
Ameerkhan <110119104009@aalimec.ac.in>
2024-06-21 19:53:32 +0530

    Add project files.

---
+++ ASP .NET CORE MVC USING ADO.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_.NET_CORE_MVC_USING_ADO.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
